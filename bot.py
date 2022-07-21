65661e117c2fc9a36a7034b3b2eb2ea25f4532ec
a2katt <mr.a2katt@gmail.com>
2022-07-21 21:38:15 +0300

    mini update

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import logging
+import asyncio
 from config import BOT_TOKEN
-from aiogram import Bot, Dispatcher, executor
+from aiogram import Bot, Dispatcher
 from handlers.echo import register_echo
 from handlers.user import register_user_start
 
@@ -10,13 +11,14 @@ def register_all_handlers(dp):  # Регистрация всех хендлер
     register_echo(dp)
 
 
-def main():
+async def main():
     bot = Bot(token=BOT_TOKEN, parse_mode='HTML')
     dp = Dispatcher(bot)
     logging.basicConfig(level=logging.INFO)
     register_all_handlers(dp)   # Запуск всех хендлеров
-    executor.start_polling(dp)
+    await dp.start_polling(bot)
 
 
 if __name__ == '__main__':  # Запуск бота
-    main()
+    asyncio.run(main())
+
