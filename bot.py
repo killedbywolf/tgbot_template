38200adadf8d29f25f5bb339404a31d7eca08432
a2katt <mr.a2katt@gmail.com>
2022-07-21 23:31:24 +0300

    mini update

--- bot.py
+++ bot.py
@@ -6,6 +6,9 @@ from handlers.echo import register_echo
 from handlers.user import register_user_start
 
 
+logger = logging.getLogger(__name__)
+
+
 def register_all_handlers(dp):  # Регистрация всех хендлеров (порядок имеет значение)
     register_user_start(dp)
     register_echo(dp)
@@ -14,11 +17,16 @@ def register_all_handlers(dp):  # Регистрация всех хендлер
 async def main():
     bot = Bot(token=BOT_TOKEN, parse_mode='HTML')
     dp = Dispatcher(bot)
+    register_all_handlers(dp)
+
     logging.basicConfig(level=logging.INFO)
-    register_all_handlers(dp)   # Запуск всех хендлеров
+
     await dp.start_polling(bot)
 
 
 if __name__ == '__main__':  # Запуск бота
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except (RuntimeError, KeyboardInterrupt):
+        logger.error('Bot stoped!')
 
